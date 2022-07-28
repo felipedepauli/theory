be5be10d856970c1fd67272aedb23bf212531945
Felipe Camargo de Pauli <fcdp83@gmail.com>
2022-07-28 13:51:36 -0300

    Argparse documentado

---
+++ python/99_Outros/arg_parser.py
@@ -0,0 +1,40 @@
+import argparse
+# Existem dois tipos de argumentos:
+#   1. Posicionais (obrigatórios)
+#   2. Optativos (ativam códigos caso sejam passadas)
+
+# Antes de qualquer coisa
+# Criar o parser!
+
+parser = argparse.ArgumentParser()
+
+# 1. POSICIONAIS
+# Os posisionais não possuem -- na frente do nome do argumento. Você pode dar o nome que quiser, já que o primeiro argumento será atribuído ao primeiro add_argument, e segundo argumento ao segundo add_argument, e assim por diante.
+
+parser.add_argument("satanas_1", help="Digite um número qualquer.", type=int)
+parser.add_argument("satanas_2", help="Digite um número qualquer.", type=int)
+
+# 2. OPTATIVOS
+# Já os optativos precisam iniciar com --. Feito isso, você pode ou não usá-los. Se eles não aparecerem no linha de comando, o interpretador não irá causar um erro.
+# Existem duas opções de argumento optativos: com argumento adicional ou sem (somente eles por eles)
+parser.add_argument("--idade", help="Digite sua idade")
+parser.add_argument("--quem", help="Testa, doidão!!", action="store_true")
+# Se quiser pode usar as short versions:
+parser.add_argument("-v", help="Verbose Mode", action="store_true")
+
+
+# Agora tá na hora de parsear
+
+args = parser.parse_args()
+
+print(f'Dae rapeize: {args.satanas_1}')
+print(f'Dae rapeize: {args.satanas_2}')
+
+if args.quem:
+    print("perguntou?")
+
+if args.idade:
+    print("Idade:", args.idade)
+
+if args.v:
+    print("Verboso é o caraleo =D")
